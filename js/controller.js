f53e33fcd2029a6f1722f0fb1e06564fec672dc3
CdrXander <xackhackman@gmail.com>
2016-12-21 10:47:57 -0700

    Some comments

--- js/controller.js
+++ js/controller.js
@@ -16,3 +16,15 @@ angular.module('quoteBook').controller('MainController', function($scope, quoteS
 
 
 })
+
+/*{"cookies":
+	{
+		"Tasty Chocolate Chip Cookie":4,
+		"Delicious Butter Pecan Cookie":3,
+		"Delectable Fortune Cookie":27,
+		"Delicious Macaroon Cookie":4,
+		"Delectable Chocolate Chip Cookie":3,
+		"Flavorful Butter Pecan Cookie":6,
+		"Tasty Macaroon Cookie":34
+	}
+}*/
